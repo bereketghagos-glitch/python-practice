a38fa1c1f1d1e1f999e0d1a9d078c45d9819e658
Bereket <bereket@gmail.com>
2026-04-23 18:50:34 -0700

    first commit

---
+++ program.py
@@ -0,0 +1,30 @@
+x = "the yellow man"
+z = 5
+y = 3
+def runTheRabbitroute():
+    global z 
+    z = 2
+    if z > y:
+        print("The orphange didn't know how to overcome his fear")
+    else:
+        print("I guess he was wrong", end = " ")
+        print(x)
+
+def iHaveaDreamToChase():
+    for i in range(3):
+        print("I have a dream to chase!")
+        print("dont stop until you win")
+        print("I am the best in the world")
+    if z > y:
+        print("I was born to conquer the world")
+    else:
+        print("Bereket Hagos stands alone at the top")
+
+
+runTheRabbitroute()
+iHaveaDreamToChase()
+
+
+
+  
+    
